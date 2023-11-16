edcba634306cfaf89542d38a345d3ad9d23eb5df
“tshittu5 <tolushittu5@gmail.com git config --global user.name tshittu5git config user.email tolushittu5@gmail.com>
2023-11-16 14:27:29 -0800

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,28 +16,30 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
+	int fd, rd, wd;
+	char *buff = malloc(sizeof(char) * letters);
 
 	if (filename == NULL)
-		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		free(buffer);
 		return (0);
 	}
 
-	free(buffer);
-	close(o);
-
-	return (w);
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	rd = read(fd, buff, letters);
+	if (rd == -1)
+	{
+		return (0);
+	}
+	wd = write(STDOUT_FILENO, buff, rd);
+	if (wd == -1)
+	{
+		return (0);
+	}
+	close(fd);
+	free(buff);
+	return (wd);
 }
